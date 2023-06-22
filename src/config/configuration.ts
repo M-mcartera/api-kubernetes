6b018fa1f4f202125021a653b9380911b2d5435f
carteramihail <mcartera47@gmail.com>
2023-06-23 00:20:46 +0300

    secure configuration:

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,15 +2,14 @@ export default () => ({
   port: parseInt(process.env.PORT) || 3000,
   FRONTEND_URL: 'http://localhost:3001',
   database: {
-    url: 'mongodb://localhost:27017',
+    url: '',
   },
   jwt: {
-    secret: 'secret123',
-    expired: '3600',
+    secret: '',
+    expired: '',
   },
   saltOrRounds: 10,
   sengrid: {
-    secure_key:
-      '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',
+    secure_key: '',
   },
 });
